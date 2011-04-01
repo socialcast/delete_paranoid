783a5bb54f18cfdce61405aebaecbcbdb32993f4
Michael Andrews <michael@socialcast.com>
2011-04-01 14:33:44 -0700

    Initial pass at test for dependent destroy

--- test/database_setup.rb
+++ test/database_setup.rb
@@ -3,10 +3,15 @@ config = YAML::load(IO.read(File.dirname(__FILE__) + '/database.yml'))
 ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + "/debug.log")
 ActiveRecord::Base.establish_connection(config[ENV['DB'] || 'sqlite'])
 
-ActiveRecord::Schema.define(:version => 1) do
+ActiveRecord::Schema.define(:version => 2) do
   create_table :blogs, :force => true do |t|
     t.column :title, :string
     t.column :body, :string
+    t.column :deleted_at, :timestamp
+  end
+  create_table :comments, :force => true do |t|
+    t.column :blog_id, :integer
+    t.column :text, :string
     t.column :deleted_at, :timestamp
   end
 end
--- test/test_delete_paranoid.rb
+++ test/test_delete_paranoid.rb
@@ -3,6 +3,8 @@ require File.join(File.dirname(__FILE__), 'helper')
 class TestDeleteParanoid < Test::Unit::TestCase
   class Blog < ActiveRecord::Base
     acts_as_paranoid
+
+    has_many :comments, :dependent => :destroy
     
     attr_accessor :called_before_destroy, :called_after_destroy, :called_after_commit_on_destroy
 
@@ -27,6 +29,10 @@ class TestDeleteParanoid < Test::Unit::TestCase
     def call_me_after_commit_on_destroy
       @called_after_commit_on_destroy = true
     end
+  end
+
+  class Comment < ActiveRecord::Base
+    acts_as_paranoid
   end
   
   context 'with paranoid class' do
@@ -55,9 +61,30 @@ class TestDeleteParanoid < Test::Unit::TestCase
       should "call after_commit_on_destroy callbacks" do
         assert @blog.called_after_commit_on_destroy
       end
-      
     end
 
     should 'hard delete with destroy!'
+
+    context 'when an instance has_many' do
+      setup do
+        @comment = Comment.create! :text => 'bar'
+        @blog = Blog.create!(:title => 'foo').tap do |blog|
+          blog.comments << @comment
+        end
+        @blog.destroy
+      end
+      should 'not be included in all results' do
+        assert !Blog.all.include?(@blog)
+        assert !Comment.all.include?(@comment)
+      end
+      should 'be included when wrapped in with_deleted block' do
+        Blog.with_deleted do
+          Comment.with_deleted do
+            assert Blog.all.include?(@blog)
+            assert Comment.all.include?(@comment)
+          end
+        end
+      end
+    end
   end
 end
