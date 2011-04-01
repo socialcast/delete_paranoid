1c05a2c8d8ae27fe29058ab5ae77bad01975b6bf
Michael Andrews <michael@socialcast.com>
2011-04-01 15:07:19 -0700

    Made dependent destroy a sub context

--- test/test_delete_paranoid.rb
+++ test/test_delete_paranoid.rb
@@ -38,30 +38,42 @@ class TestDeleteParanoid < Test::Unit::TestCase
       should "call after_commit_on_destroy callbacks" do
         assert @blog.called_after_commit_on_destroy
       end
+      should "set deleted_at" do
+        assert_not_nil @blog.deleted_at
+      end
+      context 'when an instance has dependent' do
+        setup do
+          @comment = Comment.create! :text => 'bar'
+          @blog = Blog.create!(:title => 'foo').tap do |blog|
+            blog.comments << @comment
+          end
+          @blog.destroy
+        end
+        should 'not be included in all results' do
+          assert !Comment.all.include?(@comment)
+        end
+        should 'be included when wrapped in with_deleted block' do
+          Blog.with_deleted do
+            Comment.with_deleted do
+              assert Comment.all.include?(@comment)
+            end
+          end
+        end
+        should "call before_destroy callbacks" do
+          assert @comment.called_before_destroy
+        end
+        should "call after_destroy callbacks" do
+          assert @comment.called_after_destroy
+        end
+        should "call after_commit_on_destroy callbacks" do
+          assert @comment.called_after_commit_on_destroy
+        end
+        should "set deleted_at" do
+          assert_not_nil @comment.deleted_at
+        end
+      end
     end
 
     should 'hard delete with destroy!'
-
-    context 'when an instance has_many' do
-      setup do
-        @comment = Comment.create! :text => 'bar'
-        @blog = Blog.create!(:title => 'foo').tap do |blog|
-          blog.comments << @comment
-        end
-        @blog.destroy
-      end
-      should 'not be included in all results' do
-        assert !Blog.all.include?(@blog)
-        assert !Comment.all.include?(@comment)
-      end
-      should 'be included when wrapped in with_deleted block' do
-        Blog.with_deleted do
-          Comment.with_deleted do
-            assert Blog.all.include?(@blog)
-            assert Comment.all.include?(@comment)
-          end
-        end
-      end
     end
-  end
 end
