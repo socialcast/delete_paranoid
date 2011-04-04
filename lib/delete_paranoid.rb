9c44c10b41596547410a0f69a8b1d71508519336
Geoffrey Hichborn <geoff@socialcast.com>
2011-04-04 12:26:33 -0700

    Aliased and implemented soft-delete version of Model.delete_all Added paranoid? method to Model Cleaned up tests and encourporated mandrews' association tests

--- lib/delete_paranoid.rb
+++ lib/delete_paranoid.rb
@@ -5,20 +5,39 @@ module DeleteParanoid
     def acts_as_paranoid
       class << self
         alias_method :destroy!, :destroy
+        alias_method :delete_all!, :delete_all
       end
       alias_method :destroy!, :destroy
       default_scope where(:deleted_at => nil)
-      include DeleteParanoid::InstanceMethods
       extend DeleteParanoid::ClassMethods
+      include DeleteParanoid::InstanceMethods  
+    end
+    
+    def paranoid?
+      self.included_modules.include?(InstanceMethods)
     end
   end
+  
   module ClassMethods
     def with_deleted
       self.unscoped do
         yield
       end
     end
+    
+    def delete_all(conditions = nil)
+      update_all ["deleted_at = ?", Time.now.utc], conditions
+    end
+
+    def destroy_all!(conditions = nil)
+      if conditions
+        where(conditions).destroy_all!
+      else
+        to_a.each {|object| object.destroy! }.tap { reset }
+      end
+    end
   end
+  
   module InstanceMethods
     def destroy
       if persisted?
--- test/test_delete_paranoid.rb
+++ test/test_delete_paranoid.rb
@@ -4,12 +4,13 @@ class TestDeleteParanoid < Test::Unit::TestCase
   class Blog < ActiveRecord::Base
     has_many :comments, :dependent => :destroy
     acts_as_paranoid
+    attr_accessible :title
     include CallbackTester
   end
 
   class Comment < ActiveRecord::Base
     acts_as_paranoid
-    attr_accessible :title
+    attr_accessible :text
     include CallbackTester
   end
   
@@ -23,80 +24,56 @@ class TestDeleteParanoid < Test::Unit::TestCase
         @blog = Blog.create! :title => 'foo'
         @blog.destroy
       end
-      should 'not be included in all results' do
-        assert !Blog.all.include?(@blog)
-      end
-      should 'be included when wrapped in with_deleted block' do
-        Blog.with_deleted do
-          assert Blog.all.include?(@blog)
-        end
-      end
-      should "call before_destroy callbacks" do
-        assert @blog.called_before_destroy
-      end
-      should "call after_destroy callbacks" do
-        assert @blog.called_after_destroy
-      end
-      should "call after_commit_on_destroy callbacks" do
-        assert @blog.called_after_commit_on_destroy
-      end
-      should "set deleted_at" do
-        assert_not_nil @blog.deleted_at
-      end
-      context 'when an instance has dependent' do
-        setup do
-          @comment = Comment.create! :text => 'bar'
-          @blog = Blog.create!(:title => 'foo').tap do |blog|
-            blog.comments << @comment
-          end
-          @blog.destroy
-        end
-        should 'not be included in all results' do
-          assert !Comment.all.include?(@comment)
-        end
-        should 'be included when wrapped in with_deleted block' do
-          Blog.with_deleted do
-            Comment.with_deleted do
-              assert Comment.all.include?(@comment)
-            end
-          end
-        end
-        should "call before_destroy callbacks" do
-          assert @comment.called_before_destroy
-        end
-        should "call after_destroy callbacks" do
-          assert @comment.called_after_destroy
-        end
-        should "call after_commit_on_destroy callbacks" do
-          assert @comment.called_after_commit_on_destroy
-        end
-        should "set deleted_at" do
-          assert_not_nil @comment.deleted_at
-        end
-      end
+      
+      should_soft_destroy :blog
+      should_trigger_destroy_callbacks :blog
+      should_not_trigger_update_callbacks :blog
     end
+    
+    context 'when an instance with dependents is destroyed softly' do
+      setup do
+        @comment = Comment.create! :text => 'bar'
+        @blog = Blog.create!(:title => 'foo').tap do |blog|
+          blog.comments << @comment
+        end
+        @blog.destroy
+      end
+
+      should_soft_destroy :blog
+      should_soft_destroy :comment
+      should_trigger_destroy_callbacks :blog
+      should_not_trigger_update_callbacks :blog
+      should_trigger_destroy_callbacks :comment
+      should_not_trigger_update_callbacks :comment
+    end
+    
     context "when on instance destroyed hardly" do
       setup do
         @blog = Blog.create! :title => 'foo'
         @blog.destroy!
       end
-      should 'not be included in all results' do
-        assert !Blog.all.include?(@blog)
-      end
-      should 'not be included when wrapped in with_deleted block' do
-        Blog.with_deleted do
-          assert !Blog.all.include?(@blog)
-        end
-      end
-      should "call before_destroy callbacks" do
-        assert @blog.called_before_destroy
-      end
-      should "call after_destroy callbacks" do
-        assert @blog.called_after_destroy
-      end
-      should "call after_commit_on_destroy callbacks" do
-        assert @blog.called_after_commit_on_destroy
-      end
+
+      should_hard_destroy :blog
+      should_trigger_destroy_callbacks :blog
+      should_not_trigger_update_callbacks :blog
     end
+    
+    context 'when an instance with dependents is destroyed hardly' do
+       setup do
+         @comment = Comment.create! :text => 'bar'
+         @blog = Blog.create!(:title => 'foo').tap do |blog|
+           blog.comments << @comment
+         end
+         @blog.destroy!
+       end
+
+       should_hard_destroy :blog
+       should_hard_destroy :comment
+       should_trigger_destroy_callbacks :blog
+       should_not_trigger_update_callbacks :blog
+       should_trigger_destroy_callbacks :comment
+       should_not_trigger_update_callbacks :comment
+     end
+    
   end
 end
