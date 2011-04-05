9a4833a0b7f0a5d8d7980778539bc7de619590d4
Ryan Sonnek <ryan@socialcast.com>
2011-04-05 10:50:38 -0500

    reorg tests

--- test/test_delete_paranoid.rb
+++ test/test_delete_paranoid.rb
@@ -26,9 +26,13 @@ class TestDeleteParanoid < Test::Unit::TestCase
     should 'be paranoid' do
       assert Blog.paranoid?
     end
-    context 'when an instance destroyed softly' do
+  end
+  context 'with instance of paranoid class' do
+    setup do
+      @blog = Blog.create! :title => 'foo'
+    end
+    context 'when destroying instance' do
       setup do
-        @blog = Blog.create! :title => 'foo'
         @now = Time.now.utc
         Timecop.travel @now do
           @blog.destroy
@@ -45,13 +49,39 @@ class TestDeleteParanoid < Test::Unit::TestCase
         assert_equal @now.to_i, blog.deleted_at.to_i
       end
     end
-    
-    context 'when an instance with dependents is destroyed softly' do
+    context 'when destroying instance with destroy_all' do
       setup do
-        @comment = Comment.create! :text => 'bar'
-        @blog = Blog.create!(:title => 'foo').tap do |blog|
-          blog.comments << @comment
+        Blog.destroy_all :id => @blog.id
+      end
+      should "not find instance normally" do
+        assert_raises ActiveRecord::RecordNotFound do
+          Blog.find @blog.id
         end
+      end
+      should "find instance when in with_deleted block" do
+        Blog.with_deleted do
+          assert_nothing_raised ActiveRecord::RecordNotFound do
+            Blog.find @blog.id
+          end
+        end
+      end
+    end
+    context "when destroying instance with delete_all!" do
+      setup do
+        @blog = Blog.create! :title => 'foo'
+        Blog.where({:id => @blog.id}).delete_all!
+      end
+      should_hard_destroy :blog
+    end
+  end
+
+  context 'with paranoid instance that has dependents' do
+    setup do
+      @blog = Blog.create!(:title => 'foo')
+      @comment = @blog.comments.create! :text => 'bar'
+    end
+    context 'when destroying paranoid instance' do
+      setup do
         @blog.destroy
       end
 
@@ -62,21 +92,8 @@ class TestDeleteParanoid < Test::Unit::TestCase
       should_soft_destroy :comment
       should_trigger_destroy_callbacks :comment
     end
-    
-    context "when on instance deleted hardly" do
-      setup do
-        @blog = Blog.create! :title => 'foo'
-        Blog.where({:id => @blog.id}).delete_all!
-      end
-      should_hard_destroy :blog
-    end
-    
-    context 'when an instance with dependents is destroyed hardly' do
+    context 'when destroying paranoid instance with delete_all!' do
        setup do
-         @comment = Comment.create! :text => 'bar'
-         @blog = Blog.create!(:title => 'foo').tap do |blog|
-           blog.comments << @comment
-         end
          Blog.where({:id => @blog.id}).delete_all!
        end
 
@@ -86,6 +103,5 @@ class TestDeleteParanoid < Test::Unit::TestCase
          assert_nil @comment.blog
        end
      end
-    
   end
 end
